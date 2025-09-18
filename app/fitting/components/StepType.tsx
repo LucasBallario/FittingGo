fee915544a216d72c8545daad19becafbb0cbf3e
Lucas Ballario <lucasballario@Lucass-MacBook-Air.local>
2025-09-18 19:58:40 -0400

    Start with StepType.tsx

--- app/fitting/components/StepType.tsx
+++ app/fitting/components/StepType.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default function StepType() {
+export default function StepType({ formData, setFormData }) {
+
+ const handleClick = (event) => {
+  setFormData({
+     ...formData,
+    type: event.target.value
+ })
+   
+    
+ }
+
   return (
-    <div>StepType</div>
+    <div>
+      <div>
+          <button value={'driver'} onClick={handleClick}>
+            Driver
+          </button>
+          <button value={'irons'} onClick={handleClick}>
+            irons
+          </button>
+          <button value={'wedges'} onClick={handleClick}>
+            Wedges
+          </button>
+      </div>
+    </div>
   )
 }
