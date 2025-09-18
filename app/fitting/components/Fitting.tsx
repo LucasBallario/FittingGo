05929cdc012b51568c67835ba81031acf4b6e3f8
Lucas Ballario <lucasballario@Lucass-MacBook-Air.local>
2025-09-17 20:10:32 -0400

    Fitting.tsx completed

--- app/fitting/components/Fitting.tsx
+++ app/fitting/components/Fitting.tsx
@@ -1,7 +1,69 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import StepHeader from './StepHeader'
+import StepPersonal from './StepPersonal'
+import StepPreferences from './StepPreferences'
+import StepType from './StepType'
+import StepSwing from './StepSwing'
+import Results from './Results'
+import FittingNav from './FittingNav'
+
 
 export default function Fitting() {
+    const [currentStep, setCurrentStep] = useState(0)
+
+ const steps = [
+        { id: 0, label: "Club type", component: StepType },
+        { id: 1, label: "Personal data", component: StepPersonal },
+        { id: 2, label: "Swing information", component: StepSwing },
+        { id: 3, label: "Preferences", component: StepPreferences },
+        { id: 4, label: "Results", component: Results },
+    ]
+
+    const StepComponent = steps[currentStep].component
+
+    const goNext = () => {
+        if (currentStep < steps.length -1) {
+            setCurrentStep(currentStep + 1)
+        }
+    }
+    const goBack = () => {
+        if (currentStep > 0) {
+            setCurrentStep(currentStep - 1)
+        }
+    }
+
+    const [formData,setFormData] = useState(
+          {
+            type: null,
+            handicap: null,
+            swingSpeed: null,
+            preferences: {}
+          }
+    ) 
+      
+
+   
+
   return (
-    <div>Fitting</div>
+    <div>
+        <StepHeader
+        currentStep={currentStep}
+        totalSteps={steps.length}
+        label={steps[currentStep].label}
+        />
+
+        <StepComponent 
+        formData={formData}
+        setFormData={setFormData}
+        />
+
+        <FittingNav 
+        currentStep={currentStep}
+        totalSteps={steps.length}
+        onNext={goNext}
+        onBack={goBack}
+        />
+    </div>
   )
 }
