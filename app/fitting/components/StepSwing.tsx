8dc28ba3509cc606bddde8ef8af53a1cbe568c22
Lucas Ballario <lucballario@gmail.com>
2025-09-19 11:47:38 -0400

    Start with the fitting page

---
+++ app/fitting/components/StepSwing.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StepSwing() {
+  return (
+    <div>StepSwing</div>
+  )
+}
---
+++ app/fitting/components/StepType.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StepType() {
+  return (
+    <div>StepType</div>
+  )
+}
