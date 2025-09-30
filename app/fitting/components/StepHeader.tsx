37c12b0f0dcb5cacc5c6a7fd0a328c9f5499cf80
Lucas Ballario <lucballario@gmail.com>
2025-09-30 14:20:52 -0400

    update the clubs.js dataset

--- app/fitting/components/StepHeader.tsx
+++ app/fitting/components/StepHeader.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function StepHeader() {
   return (
-    <div>StepHeader</div>
+    <div>
+      <h1 className="text-4xl font-bold">
+        Fitting<span className="text-5xl text-green-700">G</span>o
+      </h1>
+    </div>
   )
 }
